49befc04c449e40e597f2ab0c03621dcecb304bf
Bunlong <bunlong.van@gmail.com>
2020-02-03 22:55:54 +0700

    basic Q2

--- basic-questions/1/a1.java
+++ basic-questions/1/a1.java
@@ -1,35 +1,43 @@
 public class a1 {
   static int f(int[] a) {
-    int max1 = -1;
-    int max2 = -1;
+    int X = 0; // sum of the odd
+    int Y = 0; // sum of the even
     for (int i = 0; i < a.length; i++) {
-      if (a[i] > max1) {
-        max2 = max1;
-        max1 = a[i];
-      } else if (a[i] > max2 && a[i] != max1) {
-        max2 = a[i];
+      if (a[i] % 2 == 0) {
+        Y += a[i];
+      } else {
+        X += a[i];
       }
     }
-    return max2;
+    return X - Y;
   }
 
   public static void main(String args[]) {
-    int result = f(new int[]{1, 2, 3, 4});
+    int result = f(new int[]{1});
     System.out.println(result);
 
-    int result1 = f(new int[]{4, 1, 2, 3});
+    int result1 = f(new int[]{1, 2});
     System.out.println(result1);
 
-    int result2 = f(new int[]{1, 1, 2, 2});
+    int result2 = f(new int[]{1, 2, 3});
     System.out.println(result2);
 
-    int result3 = f(new int[]{1, 1});
+    int result3 = f(new int[]{1, 2, 3, 4});
     System.out.println(result3);
 
-    int result4 = f(new int[]{1});
+    int result4 = f(new int[]{3, 3, 4, 4});
     System.out.println(result4);
 
-    int result5 = f(new int[]{});
+    int result5 = f(new int[]{3, 2, 3, 4});
     System.out.println(result5);
+
+    int result6 = f(new int[]{4, 1, 2, 3});
+    System.out.println(result6);
+
+    int result7 = f(new int[]{1, 1});
+    System.out.println(result7);
+
+    int result8 = f(new int[]{});
+    System.out.println(result8);
   }
 }
--- basic-questions/README.md
+++ basic-questions/README.md
@@ -1,9 +1,35 @@
 <!-- 
+
 javac filename.java
 java filename
- -->
+
+-->
 
 1- Write a function that:
+
+* takes an array of integers as an argument
+* and returns a value based on the sums of the even and odd numbers in the array
+* let X = the sum of the odd numbers in the array
+* and let Y = the sum of the even numbers
+* The function should return X - Y
+
+The signature of the function is `int f(int[] a)`
+
+| if input array is | return |
+|:-------------|:-------------|
+| {1} | 1 |
+| {1, 2} | -1 |
+| {1, 2, 3} | 2 |
+| {1, 2, 3, 4} | -2 |
+| {3, 3, 4, 4} | -2 |
+| {3, 2, 3, 4} | 0 |
+| {4, 1, 2, 3} | -2 |
+| {1, 1} | 2 |
+| {} | 0 |
+
+Solutions:
+
+2- Write a function that:
 
 * accepts an array of non-negative integers
 * and returns the second largest integer in the array
