505b43facb4291f58a8b4399a478e3b9247bcdd0
Bunlong <bunlong.van@gmail.com>
2020-02-03 22:37:23 +0700

    basic Q1

---
+++ basic-questions/1/a1.java
@@ -0,0 +1,35 @@
+public class a1 {
+  static int f(int[] a) {
+    int max1 = -1;
+    int max2 = -1;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] > max1) {
+        max2 = max1;
+        max1 = a[i];
+      } else if (a[i] > max2 && a[i] != max1) {
+        max2 = a[i];
+      }
+    }
+    return max2;
+  }
+
+  public static void main(String args[]) {
+    int result = f(new int[]{1, 2, 3, 4});
+    System.out.println(result);
+
+    int result1 = f(new int[]{4, 1, 2, 3});
+    System.out.println(result1);
+
+    int result2 = f(new int[]{1, 1, 2, 2});
+    System.out.println(result2);
+
+    int result3 = f(new int[]{1, 1});
+    System.out.println(result3);
+
+    int result4 = f(new int[]{1});
+    System.out.println(result4);
+
+    int result5 = f(new int[]{});
+    System.out.println(result5);
+  }
+}
---
+++ basic-questions/README.md
@@ -0,0 +1,23 @@
+<!-- 
+javac filename.java
+java filename
+ -->
+
+1- Write a function that:
+
+* accepts an array of non-negative integers
+* and returns the second largest integer in the array
+* return -1 if there is no second largest
+
+The signature of the function is `int f(int[] a)`
+
+| if the input array is | return |
+|:-------------|:-------------|
+| {1, 2, 3, 4} | 3 |
+| {4, 1, 2, 3} | 3 |
+| {1, 1, 2, 2} | 1 |
+| {1, 1} | -1 |
+| {1} | -1 |
+| {} | -1 |
+
+Solutions:
